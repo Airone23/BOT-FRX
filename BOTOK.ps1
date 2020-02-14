44bc475c43fa06f5fb7c51a22e38e1890c22eb1e
Airone23 <49239648+Airone23@users.noreply.github.com>
2020-02-14 14:12:39 +0100

    Update BOTOK.ps1

--- BOTOK.ps1
+++ BOTOK.ps1
@@ -1,13 +1,47 @@
-﻿
-Import-Module "C:\lib\BOT\BOT.ps1"
-$temp = New-Item -Path "c:\" -ItemType Directory -Name "MAJBOT"
-$Temp_Script = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Airone23/BOT-FRX/master/BOTOK.ps1" -OutFile "$Temp\BOT_ameliore.ps1"
-Foreach-object {
-write-host     "╔════ Download ════╗" -fore yellow}{
-    write-host "║" -ForegroundColor Yellow -no
-    Write-Host "   Download ok!   " -ForegroundColor Blue -NoNewline
-    write-host "║" -ForegroundColor Yellow 
-}{  write-host "╚══════════════════╝"-fore yellow}
+#Script FRX_Socket 
+function global:FRX_Socket-Listen-Connect{
+    param($port=1984)
+    $socket = new-object System.Net.Sockets.TcpListener('0.0.0.0',$port)
+    if($socket -eq $null){exit 1}
+    $socket.Start()
+
+    return $socket    
+}
+function global:FRX_Socket-Listen-Read{
+    param($socket,$buffersize=512)
+    $script:client = $socket.AcceptTcpClient()
+
+    $stream = $script:client.GetStream() # je rÃ©cupÃ¨re ce qui est envoyÃ©
+    # Mais qu'est ce qui a Ã©tÃ© transmis alors ?
+    $buffer = New-Object system.byte[] $buffersize
+    do{
+        $read = $null
+        while($stream.DataAvailable -or $read -eq $null){
+            $read = $stream.read($buffer,0,$buffersize)
+        }    
+    }while($read -gt 1)
+    $message = ([System.Text.Encoding]::ASCII).GetString($buffer)
+    $message = $message.substring(0,$message.IndexOf(0))
+    
+    return $message    
+}
+function global:FRX_Socket-Listen-Close{
+    param($socket)
+    $socket.stop()
+    $script:client.close()
+    return $null
+}
+Function global:FRX_Socket-Send-Port{
+    param($port = 1984,$IPTarget=127.0.0.1,$message="echo123456")
+    $socket = new-object System.Net.Sockets.TcpClient($IPTarget, $port)
+    $data = [System.Text.Encoding]::ASCII.GetBytes($message)
+    $stream = $socket.GetStream()
+    $stream.Write($data, 0, $data.Length)
+    $socket.close()
+    return $null
+}
+$botpath = "C:\lib\BOT\BOTOK.ps1"
+$URL     = "https://raw.githubusercontent.com/Airone23/BOT-FRX/master/BOTOK.ps1"
 $botpath = "C:\lib\BOT_ameliore.ps1"
 # sur le bot : 
 function global:FRX_Socket-MessageAction{
@@ -23,7 +57,14 @@ function global:FRX_Socket-MessageAction{
         write-host "On va faire la MAJ avec ça" -ForegroundColor Magenta
         #On télécharge le fichier mis à jour
         #https://github.com/Airone23/BOT-FRX/releases/download/v1/BOT_ameliore.ps1
-        Invoke-WebRequest -Uri "" -OutFile $botpath
+        Invoke-WebRequest -Uri $URL -OutFile $botpath
+        Foreach-object {
+             write-host     "╔════ Download ════╗" -fore yellow}{
+             write-host "║" -ForegroundColor Yellow -no
+             Write-Host "   Download ok!   " -ForegroundColor Blue -NoNewline
+             write-host "║" -ForegroundColor Yellow 
+        }{   write-host "╚══════════════════╝"-fore yellow}
+
         $stop=$true
         }
         Default {write-host $message}
