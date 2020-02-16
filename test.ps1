b5d57bbaf0b42a2a4a9aaf00ae3a2320b4f69efa
Airone23 <49239648+Airone23@users.noreply.github.com>
2020-02-16 19:33:58 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,14 @@
-﻿Install-Module BurntToast
+Install-Module BurntToast
+Import-Module BurntToast
+$base64=""
+
 $Logo = "C:\$env:HOMEPATH\Pictures\logo.jfif"
+
+#on convertit l'image en base 64 en string
+$bytes = [Convert]::FromBase64String($base64)
+
+#on écrit le résultat dans un fichier dans le dossiers images
+[IO.File]::WriteAllBytes($filename, $bytes)
+
+#On déclenche la notification
 New-BurntToastNotification -AppLogo $Logo -Text "Shutdown Notification" , "Attention ça va couper ;)" -Sound Alarm6
