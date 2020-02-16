433970d4c1e28e3fde5c1f1eab14db308d0f55cd
Airone23 <49239648+Airone23@users.noreply.github.com>
2020-02-16 19:14:57 +0100

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,3 @@
+﻿Install-Module BurntToast
+$Logo = "C:\$env:HOMEPATH\Pictures\logo.jfif"
+New-BurntToastNotification -AppLogo $Logo -Text "Shutdown Notification" , "Attention ça va couper ;)" -Sound Alarm6
